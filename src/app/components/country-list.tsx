71b7064cf80a8bb847dfe4e89ea7230bb0a61b9a
pabrig <pablomartin.rigallicastillo@externos-cl.cencosud.com>
2024-03-20 17:42:03 -0300

    fix: 🐛 format key

--- src/app/components/country-list.tsx
+++ src/app/components/country-list.tsx
@@ -1,5 +1,5 @@
-'use client';
-import { useState } from 'react';
+"use client";
+import { useState } from "react";
 
 interface Country {
   name: string;
@@ -16,7 +16,7 @@ const CountryList: React.FC<CountryListProps> = ({ countries }) => {
   const toggleCountry = (countryName: string) => {
     if (selectedCountries.includes(countryName)) {
       setSelectedCountries(
-        selectedCountries.filter((country) => country !== countryName),
+        selectedCountries.filter((country) => country !== countryName)
       );
     } else {
       setSelectedCountries([...selectedCountries, countryName]);
@@ -39,18 +39,19 @@ const CountryList: React.FC<CountryListProps> = ({ countries }) => {
           type="checkbox"
           checked={selectedCountries.length === countries.length}
           onChange={toggleAllCountries}
-        />{' '}
+        />{" "}
         Select All
       </label>
       <br />
       {countries.map((country) => (
+        // eslint-disable-next-line react/jsx-key
         <div className="grid text-center">
           <label key={country.name}>
             <input
               type="checkbox"
               checked={selectedCountries.includes(country.name)}
               onChange={() => toggleCountry(country.name)}
-            />{' '}
+            />{" "}
             {country.name}
             {country.flag}
           </label>
